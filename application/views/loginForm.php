a24023f4acab5e6141ecc3ae201fba5a3572fc9f
joshgal <joga3333.jg@gmail.com>
2019-05-29 15:12:58 +0700

    Update loginForm.php
    Fixed Login Form

--- application/views/loginForm.php
+++ application/views/loginForm.php
@@ -3,6 +3,7 @@
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap-4.2.1.css')?>"/>
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap-4.2.1.css')?>"/>
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css')?>"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
@@ -19,12 +20,14 @@
       background-color: #00BCD4;
       font-weight: 550;
       font-size: 16px;
+      transition: 0.2s;
     }
     .nav-link {
       color: #00BCD4;
       border-style: solid;
       border-width: 1px;
-      border-radius: 10px;
+      font-weight: 550;
+      font-size:16px;
     }
     .masukText {
       color: #00BCD4;
@@ -33,15 +36,123 @@
       margin-left: 15px;
     }
     .kiri {
-      margin-right:30px;
+      margin-right:120px;
+      margin-left: 100px;
+      padding-right: 55px;
+      margin-top: 50px;
+    }
+    .inputUsrname {
+      width: 345px;
+      height: 50px;
+      border: 0px solid white;
+      border-radius: 10px;
+      background-color: #F3F3F3;
+      margin-bottom: 20px;
+      padding:10px;
+      color : #474747;
+    }
+
+    .inputUsrname:focus {
+      box-shadow: 0 0 2px 0 #A7A7A7;
+      transition: 0.3s;
+    }
+
+    .inputPassword {
+      width: 345px;
+      height: 50px;
+      border: 0px solid white;
+      border-radius: 10px;
+      background-color: #F3F3F3;
+      margin-bottom: 30px;
+      padding:10px;
+      color : #474747;
+    }
+
+    .inputPassword:focus {
+      box-shadow: 0 0 2px 0 #A7A7A7;
+      transition: 0.3s;
+    }
+
+    .tab-pane {
+      margin:15px;
+    }
+
+    .nav{
+      margin-right:150px;
+      margin-bottom:30px;
+      padding-right: 40px;
+    }
+
+    .btnLogin {
+      width: 345px;
+      height: 50px;
+      background-color: #00BCD4;
+      border: none;
+      border-radius: 100px;
+      color: white;
+      font-weight:550;
+      font-size:16px;
+    }
+
+    .btnLogin:hover{
+      background-color: #1A8390;
+      box-shadow: 0 0 2px 0 #1A8390;
+      transition: 0.3s;
+    }
+
+    .boundary {
+      position: relative;
+      margin-left: 120px;
+    }
+
+    .top-left{
+      position: absolute;
+      top: 8px;
+      left: 16px;
+    }
+
+    .intro{
+      top: 120px;
+      font-size: 36px;
+      width: 400px;
+      font-weight: bold;
+      color: #fff;
+      margin-left: 16px;
+    }
+
+    .logo-white{
+      top: 24px;
+      margin-left: 16px;
+    }
+
+    .inputWicon .inputUsrname{
+      padding-left: 60px;
+    }
+
+    .inputWicon .inputPassword{
+      padding-left: 60px;
+    }
+
+    .inputWicon {
+      position:relative;
+    }
+
+    .inputWicon i{
+      position:absolute;
+      left:0;
+      top:8px;
+      padding:9px 8px;
+      color : #7B7B7B;
     }
 </style>
 <body>
    <div class="bungkus">
        <div class="row">
             <div class="col-sm">
-              Halo, <br>
-              Selamat datang di website SRT
+              <div class="boundary" style="background-color: #00BCD4; height: 480px; width: 520px; border-radius: 10px">
+                <div class="top-left logo-white"><img src="<?= base_url('assets/img/logo-white.png')?>" width="250px"></div>
+                <div class="top-left intro">Halo, <br> Selamat datang di SRT</div>
+              </div>
             </div>
             <div class="col-sm kiri">
               <h1 class="masukText">Masuk</h1>
@@ -55,21 +166,29 @@
               </ul>
 
               <div class="tab-content">
-                <div class="tab-pane active" id="loginPenyewa" role="tabpanel">login buat penyewa
+                <div class="tab-pane active" id="loginPenyewa" role="tabpanel">
                     <form action="<?= base_url()?>">
-                      <input class="inputUsrname" type="text" placeholder="Username/ Email">
-                      <br>
-                      <input class="inputPassword" type="password" placeholder="Password">
-                      <br>
+                      <div class="inputWicon">
+                        <input class="inputUsrname" type="text" placeholder="Username/ Email">
+                        <i class="fa fa-user fa-lg icon"></i>
+                      </div>
+                      <div class="inputWicon">
+                        <input class="inputPassword" type="password" placeholder="Password">
+                        <i class="fa fa-lock fa-lg icon"></i>
+                      </div>
                       <button class="btnLogin" type="submit">Masuk</button>
-                    </form>  
+                    </form> 
                 </div>
-                <div class="tab-pane" id="loginPemilik" role="tabpanel">login untuk pemilik tempat
+                <div class="tab-pane" id="loginPemilik" role="tabpanel">
                     <form action="<?= base_url()?>">
-                      <input class="inputUsrname" type="text" placeholder="Username/ Email">
-                      <br>
-                      <input class="inputPassword" type="password" placeholder="Password">
-                      <br>
+                      <div class="inputWicon">
+                        <input class="inputUsrname" type="text" placeholder="Username/ Email">
+                        <i class="fa fa-user fa-lg icon"></i>
+                      </div>
+                      <div class="inputWicon">
+                        <input class="inputPassword" type="password" placeholder="Password">
+                        <i class="fa fa-lock fa-lg icon"></i>
+                      </div>
                       <button class="btnLogin" type="submit">Masuk</button>
                     </form> 
                 </div>
